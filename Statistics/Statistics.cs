c4fa344187cea80f1cb3c4d891e89d3f537a2f3f
Ayan-Liaquath <105066915+Ayan-Liaquath@users.noreply.github.com>
2022-05-06 21:20:34 +0530

    Added Stats

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Statistics
 {
     public class StatsComputer
     {
         public Stats CalculateStatistics(List<float> numbers) {
-            //Implement statistics here
+            var stats = new Stats();
+           if (numbers != null && numbers.Count > 0) {
+               stats.average = numbers.Average();
+               stats.max = numbers.Max();
+               stats.min = numbers.Min();
+           }
+           else {
+               stats.average = Double.NaN;
+               stats.max = Double.NaN;
+               stats.min = Double.NaN;
+           }
+           return stats;
         }
+    }
+        
+    public class Stats
+    {
+        public double average { get; set; }
+        public double max { get; set; }
+        public double min { get; set; }
     }
 }
